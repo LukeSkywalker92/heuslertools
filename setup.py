7174f82dcf859a27a766985522dab58b0f157b18
Lukas Scheffler <lukas.scheffler@physik.uni-wuerzburg.de>
2019-06-18 12:52:22 +0200

    test

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
+    setup_requires=['numpy']
     install_requires=['xrayutilities',
                       'matplotlib',
                       'numpy',
