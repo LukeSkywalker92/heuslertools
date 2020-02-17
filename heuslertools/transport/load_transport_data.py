4d802f806e09a8d2017c853b0dd8b7acc9fe77ea
Lukas Scheffler <lukas.scheffler@physik.uni-wuerzburg.de>
2020-02-17 08:46:55 +0100

    fix nan issue

--- heuslertools/transport/load_transport_data.py
+++ heuslertools/transport/load_transport_data.py
@@ -12,4 +12,7 @@ def load_transport_data(file, identifier):
     data = {}
 
     data = np.genfromtxt(fname=file, delimiter='\t', skip_header=search_data_start(file, identifier)-1, names=True)
+    first_line = [str(i) for i in data[0]]
+    if 'nan' in first_line:
+        data = data[1:]
     return data
