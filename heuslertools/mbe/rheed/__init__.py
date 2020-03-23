492cb3d21869b03fbb7f898b165a9f24f2b91c50
Luke <luke@lukecodewalker.de>
2020-03-23 16:03:29 +0100

    added rheed image

---
+++ heuslertools/mbe/rheed/__init__.py
@@ -0,0 +1,1 @@
+from .rheed_image import RHEEDimage
---
+++ heuslertools/mbe/rheed/rheed_image.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import PIL.ImageOps
+from skimage.measure import profile_line
+import numpy as np
+
+class RHEEDimage(object):
+
+    def __init__(self, path):
+        self.raw_image = Image.open(path)
+        print()
+        if self.raw_image.mode == 'RGB':
+            self.image = self.raw_image.convert('L')
+        else:
+            self.image = self.raw_image
+
+    def crop(self, roi):
+        self.image = self.image.crop(roi)
+
+    def invert(self):
+        self.image = PIL.ImageOps.invert(self.image)
+
+    def profile(self, src, dst, linewidth=1, order=3):
+        profile = profile_line(np.transpose(np.array(self.image)), src, dst, linewidth=linewidth, order=order)
+        len(profile)
+        return profile
