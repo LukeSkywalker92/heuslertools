2871b7cf981d1520992c1aec30ed542a1fb8139f
Luke <luke@lukecodewalker.de>
2020-08-11 16:01:48 +0200

    nimnsb stuff

--- heuslertools/xrd/__init__.py
+++ heuslertools/xrd/__init__.py
@@ -4,3 +4,4 @@ Tools for using xrayutilities for heusler stuff
 from .xrd_measurement import XRDMeasurement
 from .o2t_measurement import O2TMeasurement
 from .omega_measurement import OmegaMeasurement
+from .o2t_simulation import O2TSimulation
--- heuslertools/xrd/materials.py
+++ heuslertools/xrd/materials.py
@@ -5,6 +5,7 @@ import numpy
 from xrayutilities.materials import (Crystal, CubicAlloy, CubicElasticTensor, SGLattice)
 from xrayutilities.materials.heuslerlib import *
 from xrayutilities.materials import elements as e
+from xrayutilities.materials import InAs, GaAs
 
 def _check_elements(*elem):
     ret = []
@@ -45,6 +46,7 @@ ZnTe = Crystal("ZnTe", SGLattice(216, 6.101, atoms=[e.Zn, e.Te],
                CubicElasticTensor(82.0, 42.0, 55.0))
 
 CuMnSb = HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10))
+NiMnSb = HalfHeuslerCubic216('Ni', 'Mn', 'Sb', 5.926, cij=CubicElasticTensor(1.6707e+11, 8.206e+10, 5.327e+10))
 
 def CuMnSb(occ=[1,1,1]):
     """
@@ -52,15 +54,23 @@ def CuMnSb(occ=[1,1,1]):
     """
     return HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10), occ=occ)
 
-class CuMnSb_defect(CubicAlloy):
-    def __init__(self, occ=(1, 1, 1), defect_type=None, defec_amount=0):
+# class CuMnSb_defect(CubicAlloy):
+#     def __init__(self, occ=(1, 1, 1), defect_type=None, defec_amount=0):
+#         """
+#         This is WIP and should not be used at the moment.
+#         """
+#         if defect_type is None:
+#             CuMnSb_sto = HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10))
+#             CuMnSb_def = HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10))
+#         elif defect_type is 'Cu_Mn_SWAP':
+#             CuMnSb_sto = HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10))
+#             CuMnSb_def = HalfHeuslerCubic216('Mn', 'Sb', 'Cu', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10))
+#         super(CuMnSb_defect, self).__init__(CuMnSb_sto, CuMnSb_def, defec_amount)
+
+class InGaAs(CubicAlloy):
+
+    def __init__(self, x):
         """
-        This is WIP and should not be used at the moment.
+        In_{1-x} Ga_x As cubic compound
         """
-        if defect_type is None:
-            CuMnSb_sto = HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10))
-            CuMnSb_def = HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10))
-        elif defect_type is 'Cu_Mn_SWAP':
-            CuMnSb_sto = HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10))
-            CuMnSb_def = HalfHeuslerCubic216('Mn', 'Sb', 'Cu', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10))
-        super(CuMnSb_defect, self).__init__(CuMnSb_sto, CuMnSb_def, defec_amount)
+        super(InGaAs, self).__init__(InAs, GaAs, x)
