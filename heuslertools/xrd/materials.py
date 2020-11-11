d31ad31f260ab0a19cd8bddfb0ff5b5213c13565
Lukas Scheffler <lukas.scheffler@physik.uni-wuerzburg.de>
2020-11-11 11:54:45 +0100

    fix cumnsb lattice constant

--- heuslertools/xrd/materials.py
+++ heuslertools/xrd/materials.py
@@ -47,14 +47,14 @@ ZnTe = Crystal("ZnTe", SGLattice(216, 6.101, atoms=[e.Zn, e.Te],
                CubicElasticTensor(82.0, 42.0, 55.0))
 
 # doi:10.1002/pssc.201600182
-CuMnSb = HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10))
+CuMnSb = HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.095, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10))
 NiMnSb = HalfHeuslerCubic216('Ni', 'Mn', 'Sb', 5.926, cij=CubicElasticTensor(1.6707e+11, 8.206e+10, 5.327e+10))
 
-def CuMnSb(occ=[1,1,1]):
+def CuMnSb_occ(occ=[1,1,1]):
     """
     Returns a CuMnSb Material
     """
-    return HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.09, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10), occ=occ)
+    return HalfHeuslerCubic216('Cu', 'Mn', 'Sb', 6.095, cij=CubicElasticTensor(1.056e+11, 8.27e+10, 5.47e+10), occ=occ)
 
 # class CuMnSb_defect(CubicAlloy):
 #     def __init__(self, occ=(1, 1, 1), defect_type=None, defec_amount=0):
